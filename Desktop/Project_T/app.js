0d680f563d4cbeed13a29db57e25e3cd72a5324e
abishekpuri <apuri@connect.ust.hk>
2014-11-03 20:20:28 +0800

    Added server

--- Desktop/Project_T/app.js
+++ Desktop/Project_T/app.js
@@ -56,5 +56,17 @@ app.use(function(err, req, res, next) {
     });
 });
 
+var http = require('http');
+
+var server = http.createServer(function(req, res){
+
+});
+
+server.listen('3000');
+
+
+			       
+    
+
 
 module.exports = app;
